9546d7a2fd88206a43f8fb0059b17aaa40928c9a
Juan Giménez <juanp.gi2485@gmail.com>
2022-03-16 14:11:22 +0000

    New cow-say

---
+++ index.js
@@ -0,0 +1,9 @@
+const userInfo = require('./information');
+
+const cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : `Hello I´m ${userInfo.name} from ${userInfo.campus} Campus!`,
+    e : "oO",
+    T : "U "
+}));
---
+++ information.js
@@ -0,0 +1,9 @@
+const info = {
+    studentName : "Juan",
+    campus : "Lisbon"
+};
+
+module.exports = {
+    name: info.studentName,
+    campus: info.campus,
+};
